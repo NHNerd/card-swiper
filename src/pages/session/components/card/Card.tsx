8081c6cc9f88d2c689a22fe7339d5723d1bff54b
NHNerd <promenader.off@gmail.com>
2024-06-16 16:58:19 +0300

    - decompositing card.jsx (I did make it little bit weird, I think exists a better way )

--- src/pages/session/components/card/Card.tsx
+++ src/pages/session/components/card/Card.tsx
@@ -1,68 +1,61 @@
 import React from 'react';
-
 import { useUiState } from '../../../../zustand';
-
+import {
+  onMouseDownDragLogic,
+  onMouseUpDragLogic,
+  onMouseMoveDragLogic,
+  cardEventAddListners,
+  cardEventCleanupListners,
+} from './handlers/cardMoveLogic';
 import cssCard from './Card.module.css';
 
-type Props = {};
+type Props = {
+  //! надо задать точнее (may be...)
+  ContainerSessionRef: object;
+};
 
 export default function Card({ ContainerSessionRef }: Props) {
   const { page, setPage } = useUiState();
+
   const cardRef = React.useRef<HTMLButtonElement>(null);
   const isClicked = React.useRef<boolean>(false);
 
-  const [coordsDown, setCoordsDown] = React.useState({ x: 0, y: 0 });
-
-  const [cardCurrentPos, setCardCurrentPos] = React.useState({ x: 0, y: 0 });
-  const [cardCurrentPosPrev, setCardCurrentPosPrev] = React.useState({ x: 0, y: 0 });
+  const [coordsDown, setCoordsDown] = React.useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [cardCurrentPos, setCardCurrentPos] = React.useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [cardCurrentPosPrev, setCardCurrentPosPrev] = React.useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
 
   React.useEffect(() => {
-    if (!cardRef.current || !ContainerSessionRef.current) return;
+    if (!cardRef.current && !ContainerSessionRef.current) return;
 
     const card = cardRef.current;
     const containerSession = ContainerSessionRef.current;
 
     const onMouseDown = (e: MouseEvent) => {
-      e.preventDefault();
-      isClicked.current = true;
-
-      setCoordsDown({ x: e.clientX, y: e.clientY });
+      onMouseDownDragLogic(e, isClicked, setCoordsDown);
     };
-
     const onMouseUp = (e: MouseEvent) => {
-      e.preventDefault();
-      isClicked.current = false;
-
-      setCardCurrentPosPrev(cardCurrentPos);
+      onMouseUpDragLogic(e, isClicked, setCardCurrentPosPrev, cardCurrentPos);
     };
-
     const onMouseMove = (e: MouseEvent) => {
-      e.preventDefault();
-      if (!isClicked.current) return;
-
-      const moveFromZero = { x: e.clientX - coordsDown.x, y: e.clientY - coordsDown.y };
-      setCardCurrentPos({
-        x: moveFromZero.x + cardCurrentPosPrev.x,
-        y: moveFromZero.y + cardCurrentPosPrev.y,
-      });
-
-      //Style
-      card.style.translate = `${cardCurrentPos.x}px ${cardCurrentPos.y}px`;
+      onMouseMoveDragLogic(
+        e,
+        isClicked,
+        coordsDown,
+        cardCurrentPos,
+        setCardCurrentPos,
+        cardCurrentPosPrev,
+        card
+      );
     };
 
-    card?.addEventListener('mousedown', onMouseDown);
-    card?.addEventListener('mouseup', onMouseUp);
-    containerSession?.addEventListener('mousemove', onMouseMove);
-    containerSession?.addEventListener('mouseleave', onMouseUp);
+    const cardProps = { card, containerSession, onMouseDown, onMouseUp, onMouseMove };
+    // Here is avoiding listers & cleanup
+    cardEventAddListners({ ...cardProps });
 
-    const cleanup = () => {
-      card?.removeEventListener('mousedown', onMouseDown);
-      card?.removeEventListener('mouseup', onMouseUp);
-      containerSession?.removeEventListener('mousemove', onMouseMove);
-      containerSession?.removeEventListener('mouseleave', onMouseUp);
-    };
-
-    return cleanup;
+    return () => cardEventCleanupListners({ ...cardProps });
   }, [coordsDown, cardCurrentPos]);
 
   return (
